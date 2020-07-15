a8ec4a68be3ecadec58285223040286b3af2dcdf
nguyendotnet <ndpn.dev@gmail.com>
2020-07-15 09:44:21 +0700

    up

---
+++ main.js
@@ -0,0 +1,31 @@
+var input = document.getElementById("nameUser");
+
+var button = document.getElementById("submit");
+
+function createElement() {
+    var li = document.createElement("li");
+    var textNode = document.createTextNode(nameUser.value);
+    var ul = document.querySelector("ul");
+    li.appendChild(textNode);
+    ul.appendChild(li);
+}
+
+function condition() {
+    return nameUser.value.length;
+}
+
+function afterClick() {
+
+    if (condition() > 0) {
+        createElement();
+    }
+}
+button.addEventListener("click", afterClick);
+
+function afterkeyClick(event) {
+
+    if (condition() > 0 && event.keyCode === 13) {
+        createElement();
+    }
+}
+input.addEventListener("keypress", afterkeyClick);
